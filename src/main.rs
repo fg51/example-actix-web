3d702c22503539253de3cd3e96f5ebee5e824ace
kflange <zknsftk1msrk@gmail.com>
2019-09-02 16:54:06 +0900

    create minimum server.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::env;
+
+use env_logger;
+use log::info;
+
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+
 fn main() {
-    println!("Hello, world!");
+    env::set_var("RUST_LOG", "info");
+    env_logger::init();
+
+    info!("actix-web run");
+
+    HttpServer::new(|| App::new().route("/", web::to(index)))
+        .bind("127.0.0.1:8080")
+        .unwrap()
+        .run()
+        .unwrap();
+}
+
+fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
 }
