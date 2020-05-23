b74aca3794c0c2b12d5c5d2aacc1aa26d97bff0b
kflange <zknsftk1msrk@gmail.com>
2020-05-24 00:53:11 +0900

    Changed: actix-web 1.* to 2.*

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,37 @@ use env_logger;
 use log::info;
 
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use actix_web::get;
 
-fn main() {
-    env::set_var("RUST_LOG", "info");
+#[actix_rt::main]
+async fn main() -> std::io::Result<()>{
+    let port = 8080;
+
+    env::set_var("RUST_LOG", "actix_web=info");
     env_logger::init();
 
     info!("actix-web run");
 
-    HttpServer::new(|| App::new().route("/", web::to(index)))
-        .bind("127.0.0.1:8080")
-        .unwrap()
-        .run()
-        .unwrap();
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(index))
+            .route("/again", web::get().to(index2))
+            .service(index3)
+    })
+    .bind(("127.0.0.1", port))?
+    .run()
+    .await
 }
 
-fn index() -> impl Responder {
+async fn index() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
+}
+
+async fn index2() -> impl Responder {
+    HttpResponse::Ok().json("{\"message\":\"Hello world again!\"}")
+}
+
+#[get("/macro-path")]
+async fn index3() -> impl Responder {
+    HttpResponse::Ok().body("this is index3")
 }
