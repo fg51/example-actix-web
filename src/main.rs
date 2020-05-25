a155674aa7a4dce7d6c23cf0d7304033db254897
kflange <zknsftk1msrk@gmail.com>
2020-05-25 09:34:31 +0900

    Added: graphql

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
 use std::env;
+use std::sync::Arc;
 
 use env_logger;
 use log::info;
 
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, Error, HttpResponse, HttpServer, Responder};
 use actix_web::get;
+
+use juniper::http::{graphiql::graphiql_source, GraphQLRequest};
+
+mod graphql;
+use graphql::schema::{create_schema, Context, Fruit, Schema};
+
+
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()>{
@@ -13,13 +21,18 @@ async fn main() -> std::io::Result<()>{
     env::set_var("RUST_LOG", "actix_web=info");
     env_logger::init();
 
+    let schema = std::sync::Arc::new(create_schema());
+
     info!("actix-web run");
 
-    HttpServer::new(|| {
+    HttpServer::new(move || {
         App::new()
+            .data(schema.clone())
             .route("/", web::get().to(index))
             .route("/again", web::get().to(index2))
             .service(index3)
+            .service(graphiql)
+            .route("/graphql", web::post().to(graphql))
     })
     .bind(("127.0.0.1", port))?
     .run()
@@ -38,3 +51,36 @@ async fn index2() -> impl Responder {
 async fn index3() -> impl Responder {
     HttpResponse::Ok().body("this is index3")
 }
+
+
+async fn graphql(
+    st: web::Data<Arc<Schema>>,
+    data: web::Json<GraphQLRequest>,
+) -> Result<HttpResponse, Error> {
+    let user = web::block(move || {
+        let res = data.execute(
+            &st,
+            &Context {
+                fruits: vec![
+                    Fruit::new("1".to_string(),"apple".to_string(),"America".to_string(),
+                    ),
+                    Fruit::new("2".to_string(), "banana".to_string(), "Brazil".to_string()),
+                    Fruit::new("3".to_string(), "candy".to_string(), "China".to_string()),
+
+                ],
+            }
+        );
+        Ok::<_, serde_json::error::Error>(serde_json::to_string(&res)?)
+    })
+    .await?;
+    Ok(HttpResponse::Ok().content_type("application/json")
+        .body(user))
+}
+
+#[get("/graphiql")]
+pub async fn graphiql() -> HttpResponse {
+    let html = graphiql_source("http://localhost:8080/graphql");
+    HttpResponse::Ok().content_type("text/html; charset=utf-8")
+        .body(html)
+}
+
